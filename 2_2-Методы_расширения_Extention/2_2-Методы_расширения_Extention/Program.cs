9c38f639f8f7c6c97439e9ab755c21ea6b0f10bf
Дмитрий Прилепко <54925901+Arhivater@users.noreply.github.com>
2023-03-10 18:24:44 +0700

    Add files via upload

---
+++ 2_2-Методы_расширения_Extention/2_2-Методы_расширения_Extention/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_2_Методы_расширения_Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            string Num = Console.ReadLine();
+            String answer = StrExtension.StrPalindrome(Num);
+            Console.Write(answer);
+            Console.Read();
+            Environment.Exit(0);
+        }
+    }
+
+    public static class StrExtension
+    {
+        public static string StrPalindrome(this string number)
+        {
+            string answer_Extension = number.ToLower().SequenceEqual(number.ToLower().Reverse())
+             ? answer_Extension = number + " is palindrome" : answer_Extension = number + " not a palindrome";
+
+            return answer_Extension;
+        }
+    }
+}
+
+// Console.WriteLine("Hallo Welt!");
